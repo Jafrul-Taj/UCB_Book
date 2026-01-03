78480430c4b64fbb7e11ac2561dfd7d04982b3e5
Jafrul-Taj <jfhasan210@gmail.com>
2026-01-04 00:17:46 +0600

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +19,22 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var tokenkey = builder.Configuration["TokenKey"]
+                    ?? throw new Exception("Token key not found - program.cs");
 
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+               ValidateIssuerSigningKey = true,
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenkey)),
+               ValidateIssuer = false,
+               ValidateAudience =false  
+            };
+        });
+
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+ 
 
 app.MapControllers();
 
@@ -33,4 +53,7 @@ app.UseCors(policy => policy.AllowAnyHeader()
                             .AllowAnyMethod()
                             .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
