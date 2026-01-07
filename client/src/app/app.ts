b486a3239f850e25962e4f5342ee7141d34f93d7
Jafrul-Taj <jfhasan210@gmail.com>
2026-01-08 01:09:25 +0600

    persisting login user updated

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -19,8 +19,9 @@ export class App implements  OnInit{
 
 
   async ngOnInit() {
-    this.members.set(await this.getMembers());
     this.setCurrentUser();
+    this.members.set(await this.getMembers());
+    
   }
 
   setCurrentUser(){
