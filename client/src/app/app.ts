478a2502925851357d1808ada22c9655fc11867d
Jafrul-Taj <jfhasan210@gmail.com>
2026-01-08 01:06:22 +0600

    persisting login user

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { Nav } from "../layout/nav/nav";
 })
 export class App implements  OnInit{
   
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = signal('Ucb Book');
   protected members = signal<any>([]);
@@ -18,8 +20,16 @@ export class App implements  OnInit{
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
   }
 
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
    async getMembers() {
    try {
 
