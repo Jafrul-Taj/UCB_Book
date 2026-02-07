b24962312b3d5b5a49ee2ad750b4875933ad42b6
Jafrul-Taj <jfhasan210@gmail.com>
2026-02-07 16:09:22 +0600

    End of section 12

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,36 +1,97 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output, signal } from '@angular/core';
+import {  AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
 import { ToastService } from '../../../core/services/toast-service';
+import { JsonPipe } from '@angular/common';
+import { TextInput } from "../../../shared/text-input/text-input";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register  {
+  
   private accountService = inject(AccountService);
+  private router = inject(Router);
   cancelRegister= output<boolean>();
   protected creds = {} as RegisterCreds;
+  private fb = inject(FormBuilder)
   private toast= inject(ToastService);
+  protected profileForm: FormGroup;
+  protected credentialForm: FormGroup;
+  protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
 
-  register() {
-    // console.log('Registering user with credentials:', this.creds);
-    this.accountService.register(this.creds).subscribe({
-      next: user => {
-        console.log('User registered successfully:', user);
-        this.cancelRegister.emit(false);
-        this.toast.success('Registration completed successfully.');
-      },
-      error: error => {
-        console.error('Registration failed:', error);
-        this.toast.error( error.error); 
-      }
-    });
+  constructor(){
+     this.credentialForm = this.fb.group({
+      email: ['', [Validators.required,Validators.email]],
+      displayName: ['',[Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(8)]],
+      confirmPassword: ['',[Validators.required, this.matchValues('password')]]
+    })
+    this.credentialForm.controls['password'].valueChanges.subscribe(()=>{
+      this.credentialForm.controls['confirmPassword'].updateValueAndValidity();
+    })
+
+    this.profileForm = this.fb.group({
+      gender:['male', Validators.required],
+      dateOfBirth:['', Validators.required],
+      city:['', Validators.required],
+      country:['', Validators.required],
+  })
   }
 
+  nextStep(){
+    if(this.credentialForm.valid){
+      this.currentStep.update(prevStep => prevStep+1);
+    }
+  }
+
+  
+  prevStep(){
+    this.currentStep.update(prevStep => prevStep-1);
+  }
+
+
+  register(){
+    if(this.profileForm.valid && this.credentialForm.valid){
+      const formData = {...this.credentialForm.value, ...this.profileForm.value};
+      this.accountService.register(formData).subscribe({
+        next: () => {
+            this.router.navigateByUrl('/members');
+            //this.cancel();
+        },
+        error: error =>{
+          console.log(error);
+          this.validationErrors.set(error);
+        } 
+      }) 
+    }
+  }
+  getMaxDate(){
+    const today = new Date();
+    today.setFullYear(today.getFullYear()-18);
+    return today.toISOString().split('T')[0];
+  }
+
+  matchValues(matchTo: string): ValidatorFn 
+  {
+    return(control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+
+      if(!parent) return null;
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : {passwordMismatch:true}
+
+    }
+  }
+
+  
+  
   cancel() {
     this.cancelRegister.emit(false);
   }
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -15,4 +15,8 @@ export type RegisterCreds = {
   email: string;
   displayName: string;
   password: string;
+  gender: string;
+  city: string;
+  dateOfBirth:string;
+  country:string;
 };
