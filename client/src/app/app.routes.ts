bf674aff35a421f4623570dd1db367ae4454194e
“Jafrul-Taj” <jfhasan210@gmail.comgit config --global user.name Jafrul-Tajgit config --global user.email jfhasan210@gmail.com>
2026-03-01 22:56:08 +0600

    Adding an admin guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { memberResolver } from '../features/members/member-resolver';
 import { MemberProfile } from '../features/members/member-profile/member-profile';
 import { preventUnsavedChangesGuard } from '../core/guards/prevent-unsaved-changes-guard';
 import { Admin } from '../features/admin/admin';
+import { adminGuard } from '../core/guards/admin-guard';
 
 export const routes: Routes = [
     {path: '', component: Home },
@@ -38,7 +39,7 @@ export const routes: Routes = [
              },
             {path: 'lists', component: Lists },
             {path: 'messages', component: Messages },
-            {path: 'admin', component: Admin }
+            {path: 'admin', component: Admin, canActivate: [adminGuard]}
         ]
     },
     {path: 'errors', component: TestErrors },
