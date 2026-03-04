738a4c39b01ef03b9084b6dc2c9d932c48bb243f
“Jafrul-Taj” <jfhasan210@gmail.comgit config --global user.name Jafrul-Tajgit config --global user.email jfhasan210@gmail.com>
2026-03-05 02:58:51 +0600

    Getting the user roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,17 +1,34 @@
 using System;
+using System.Threading.Tasks;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequiredAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
-        return Ok("Only admins can see this.");
+        var users = await userManager.Users.ToListAsync();
+        var userList = new List<object>();
+        foreach (var user in users)
+        {
+            var roles = await userManager.GetRolesAsync(user);
+            userList.Add(new
+            {
+                 user.Id,
+                 user.UserName,
+                 Roles = roles.ToList()
+            });
+        }
+        return Ok(userList);
     }
+    
 
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
